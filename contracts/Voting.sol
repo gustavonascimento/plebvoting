fe1206983e9fdf8946c61fa0acfddca77fa84be7
Gustavo Moreira <gustavomn93@gmail.com>
2018-07-16 21:21:55 -0300

    Starting PlebVoting

---
+++ contracts/Voting.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.4.24;
+
+contract Voting {
+    // Stote vote
+
+    // Read vote
+    string public vote;
+
+    // Constructor
+    function Voting () public {
+        vote = "Sim";
+    }
+}
